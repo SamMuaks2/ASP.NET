6acb58b91b0a623470428e21249ebe5de0fa71cc
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-23 11:43:25 +0100

    Start Todo list

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null ? TypedResults.NotFound() : TypedResults.Ok(targetTodo);
+});
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todod/{id}", task);
+});
 
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted)
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa669e709889e3140f07229f75a8fe02547f67ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485f74a32d6846cae2cddbaac71a80b4d2c08383")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
