8ff57d0656e9ba37361c5faf952117a105bde124
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-11-11 10:19:15 +0000

    Add endpoints filter

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5c38a7f4c48e2e17bb1fb4aed2a2b2a3f99c1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b0a7b7a84333e4049080e49cdc41c38323e9c6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
